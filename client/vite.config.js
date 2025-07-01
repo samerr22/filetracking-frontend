250ec94a666325f951a7157af9462d2b5069260b
samerr22 <laksameera5@gmail.com>
2025-07-01 15:01:11 +0530

    done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
-    proxy: {
-      '/api': 
-      {
-        target: 'http://localhost:3000',
-        secure: false,
-      },
+    fs: {
+      allow: ['..'],
     },
   },
-  plugins: [react()],
-})
+});
+
